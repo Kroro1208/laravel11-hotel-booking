2e5a942d1323b5f9343cf4e87e886882be4b1b6d
Kroro1208 <Kroro1208@users.noreply.github.com>
2024-07-15 09:29:32 +0000

    Apply PHP CS Fixer and Larastan fixes

--- app/Http/Controllers/ReservationAreaController.php
+++ app/Http/Controllers/ReservationAreaController.php
@@ -3,14 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\ReservationArea;
-use Illuminate\Http\Request;
 
 class ReservationAreaController extends Controller
 {
     public function reservationArea(ReservationArea $reservationArea)
     {
         return view('backend.reservation.area', [
-            'reservationArea' => $reservationArea
+            'reservationArea' => $reservationArea,
         ]);
     }
 }
