7fa0dfa97eef26de3b9e3138373323804b077ef3
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-07-27 19:25:25 +0900

    FormRequest修正

--- app/Http/Requests/PlanStoreRequest.php
+++ app/Http/Requests/PlanStoreRequest.php
@@ -20,20 +20,3 @@ class PlanStoreRequest extends FormRequest
         ];
     }
 }
-
-class PlanUpdateRequest extends FormRequest
-{
-    public function rules(): array
-    {
-        return [
-            'title' => 'required|string|max:255',
-            'description' => 'required|string',
-            'start_date' => 'required|date',
-            'end_date' => 'required|date|after_or_equal:start_date',
-            'images' => 'sometimes|array',
-            'images.*' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
-            'room_types' => 'required|array',
-            'room_types.*' => 'exists:room_types,id',
-        ];
-    }
-}
